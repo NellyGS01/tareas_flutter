1a30d8aaca2b855318c4cc2fb724d8887c0f5802
NellyGS01 <100099928+NellyGS01@users.noreply.github.com>
2023-09-01 20:28:41 -0600

    Detalles menores

--- tarea_2_5/lib/my_app.dart
+++ tarea_2_5/lib/my_app.dart
@@ -9,8 +9,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Botonesyotros(),
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const Botonesyotros(),
     );
   }
 }
@@ -23,19 +26,67 @@ class Botonesyotros extends StatefulWidget {
 }
 
 class _MainApp extends State<Botonesyotros> {
+  //controladores
   var dineroController = TextEditingController();
+
+  //Variables
   bool? genero;
+  var gender = '';
+
+  int redondoF = 0;
 
   var total = '';
+  bool light = true;
+  bool redondo = true;
 
-  //final String _15 = '''Tienes que pagar esto de propina con el 15%''';
-
+  //Funciones
   void _setText() {
     setState(() {
-      double propina = double.parse(dineroController.text);
-      propina = propina * 2;
+      //funcion para el 20%
+      if (gender == '20') {
+        if (redondo == true) {
+          double propina = double.parse(dineroController.text);
+          propina = propina * 1.2;
 
-      total = propina.toString();
+          redondoF = propina.round();
+          total = redondoF.toStringAsFixed(1);
+        } else {
+          double propina = double.parse(dineroController.text);
+          propina = propina * 1.2;
+
+          total = propina.toStringAsFixed(2);
+        }
+      }
+      //funcion para el 18%
+      if (gender == '18') {
+        if (redondo == true) {
+          double propina = double.parse(dineroController.text);
+          propina = propina * 1.18;
+
+          redondoF = propina.round();
+          total = redondoF.toStringAsFixed(1);
+        } else {
+          double propina = double.parse(dineroController.text);
+          propina = propina * 1.18;
+
+          total = propina.toStringAsFixed(2);
+        }
+      }
+      //funcion para el 15%
+      if (gender == '15') {
+        if (redondo == true) {
+          double propina = double.parse(dineroController.text);
+          propina = propina * 1.15;
+
+          redondoF = propina.round();
+          total = redondoF.toStringAsFixed(1);
+        } else {
+          double propina = double.parse(dineroController.text);
+          propina = propina * 1.15;
+
+          total = propina.toStringAsFixed(2);
+        }
+      }
     });
   }
 
@@ -50,7 +101,7 @@ class _MainApp extends State<Botonesyotros> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 17, 129, 19),
+        backgroundColor: Theme.of(context).colorScheme.primary,
         title: const Text(
           'Tip Time',
           style: TextStyle(color: Colors.white),
@@ -61,29 +112,129 @@ class _MainApp extends State<Botonesyotros> {
         child: Column(
           children: <Widget>[
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                IconButton(
-                    onPressed: () {
-                      genero = false;
-                      setState(() {});
-                    },
-                    icon: Icon(
-                      Icons.male,
-                      color: genero == false ? Colors.indigo : Colors.grey,
-                    ))
+                const SizedBox(
+                  width: 15,
+                ),
+                Expanded(
+                  child: TextField(
+                    controller: dineroController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                        icon: Icon(Icons.home),
+                        labelText: "Ingresa el costo total",
+                        border: OutlineInputBorder()),
+                  ),
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
               ],
             ),
-            TextField(
-              controller: dineroController,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                  icon: Icon(Icons.home),
-                  labelText: "Ingresa el costo total",
-                  border: OutlineInputBorder()),
+            const ListTile(
+              title: Text(
+                "¿Como estuvo el servicio? ",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              leading: Column(
+                children: [
+                  Expanded(
+                      child: Icon(
+                    Icons.dinner_dining_sharp,
+                    color: Colors.green,
+                  ))
+                ],
+              ),
             ),
-            MaterialButton(onPressed: _setText, child: const Text("Calcular")),
-            Text(total)
+            RadioListTile(
+              title: const Text("Amazing(20%)"),
+              value: "20",
+              groupValue: gender,
+              onChanged: (value) {
+                setState(() {
+                  gender = value.toString();
+                });
+              },
+            ),
+            RadioListTile(
+              title: const Text("Good(18%)"),
+              value: "18",
+              groupValue: gender,
+              onChanged: (value) {
+                setState(() {
+                  gender = value.toString();
+                });
+              },
+            ),
+            RadioListTile(
+              title: const Text("Okay(15%)"),
+              value: "15",
+              groupValue: gender,
+              onChanged: (value) {
+                setState(() {
+                  gender = value.toString();
+                });
+              },
+            ),
+            ListTile(
+              leading: const Column(
+                children: [
+                  Expanded(
+                      child: Icon(
+                    Icons.credit_card,
+                    color: Colors.green,
+                  ))
+                ],
+              ),
+              title: const Text(
+                "Round up tip",
+              ),
+              trailing: Column(
+                children: [
+                  Expanded(
+                    child: Switch(
+                      // This bool value toggles the switch.
+                      value: light,
+
+                      onChanged: (bool value) {
+                        // This is called when the user toggles the switch.
+                        setState(() {
+                          redondo = value;
+                          light = value;
+                        });
+                      },
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Row(
+              children: [
+                const SizedBox(
+                  width: 23,
+                ),
+                Expanded(
+                  child: ElevatedButton(
+                      onPressed: _setText, child: const Text("Calcular")),
+                ),
+                const SizedBox(
+                  width: 23,
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Row(
+                  children: [
+                    const SizedBox(
+                      width: 234,
+                    ),
+                    Text('El total es de : \$$total'),
+                  ],
+                ),
+              ],
+            ),
           ],
         ),
       ),
